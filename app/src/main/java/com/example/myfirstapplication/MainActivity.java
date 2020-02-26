e94c1950d1991678dfc13704ef334c2a8e0fcbdf
Abenezer Atnafu <se.abenezer.atnafu@gmail.com>
2020-02-26 10:15:14 +0300

    App is created using Android studio for the sake of class

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("Hello World");
+
+        setContentView(R.layout.activity_main);
+    }
+}
